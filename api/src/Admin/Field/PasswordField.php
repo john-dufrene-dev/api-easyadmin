7d06ad9fa5ebc4ce467e69b3df6e39bbaa74a9a2
John Dufrene <john.dufrene.pro@gmail.com>
2020-08-26 10:51:49 +0200

    Add default Password field

---
+++ api/src/Admin/Field/PasswordField.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Admin\Field;
+
+use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+
+final class PasswordField implements FieldInterface
+{
+    use FieldTrait;
+
+    public static function new(string $propertyName, ?string $label = null): self
+    {
+        return (new self())
+            ->setProperty($propertyName)
+            ->setLabel($label)
+            ->setTemplatePath('admin/field/password.html.twig')
+            ->setFormType(PasswordType::class);
+    }
+}
