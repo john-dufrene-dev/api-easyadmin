e597e1ce10d5b17a5cf1afd25c0103401bee7a60
John Dufrene <john.dufrene.pro@gmail.com>
2020-08-27 16:52:27 +0200

    Add permissions service for Admin and AdminGroup + news methods

--- api/src/Service/Admin/Permissions/PermissionsAdmin.php
+++ api/src/Service/Admin/Permissions/PermissionsAdmin.php
@@ -4,15 +4,91 @@ namespace App\Service\Admin\Permissions;
 
 final class PermissionsAdmin
 {
+    /**
+     * 
+     * Role super admin
+     * You can do everythinks you want with this role
+     *
+     */
+    public const
+        DEFAULT = 'ROLE_ADMIN';
+    /**
+     * 
+     * Role super admin
+     * You can do everythinks you want with this role
+     *
+     */
     public const IS_ADMIN = 'ROLE_SUPER_ADMIN';
 
-    // Role actions for Admin 
-    public const ROLE_ADMIN_ACTION_ALL      = 'ROLE_ADMIN_ACTION_ALL';
-    public const ROLE_ADMIN_ACTION_INDEX    = 'ROLE_ADMIN_ACTION_INDEX';
-    public const ROLE_ADMIN_ACTION_EDIT     = 'ROLE_ADMIN_ACTION_EDIT';
-    public const ROLE_ADMIN_ACTION_DELETE   = 'ROLE_ADMIN_ACTION_DELETE';
-    public const ROLE_ADMIN_ACTION_NEW      = 'ROLE_ADMIN_ACTION_NEW';
-    public const ROLE_ADMIN_ACTION_DETAIL   = 'ROLE_ADMIN_ACTION_DETAIL';
+    /**
+     * 
+     * Role allowed to edit roles
+     * You can change roles of all Admin with this permissions
+     *
+     */
+    public const ROLE_ALLOWED_TO_EDIT_ROLES     = 'ROLE_ALLOWED_TO_EDIT_ROLES';
+
+    /**
+     * 
+     * Role allowed to edit groups
+     * You can change groups of all Admin with this permissions
+     *
+     */
+    public const ROLE_ALLOWED_TO_EDIT_GROUPS     = 'ROLE_ALLOWED_TO_EDIT_GROUPS';
+
+    /**
+     * 
+     * Role allowed to switch
+     * You can switch and test other Admin
+     *
+     */
+    public const ROLE_ALLOWED_TO_SWITCH     = 'ROLE_ALLOWED_TO_SWITCH';
+
+    /**
+     * 
+     * Roles actions
+     * You can access actions depends of your roles
+     *
+     */
+
+    /*************** -- ROLES ACTIONS FOR ADMIN ENTITY -- ***************/
+    public const ROLE_ADMIN_ACTION_ALL          = 'ROLE_ADMIN_ACTION_ALL';
+    public const ROLE_ADMIN_ACTION_INDEX        = 'ROLE_ADMIN_ACTION_INDEX';
+    public const ROLE_ADMIN_ACTION_EDIT         = 'ROLE_ADMIN_ACTION_EDIT';
+    public const ROLE_ADMIN_ACTION_DELETE       = 'ROLE_ADMIN_ACTION_DELETE';
+    public const ROLE_ADMIN_ACTION_NEW          = 'ROLE_ADMIN_ACTION_NEW';
+    public const ROLE_ADMIN_ACTION_DETAIL       = 'ROLE_ADMIN_ACTION_DETAIL';
+
+    /*************** -- ROLES ACTIONS FOR ADMIN_GROUP ENTITY -- ***************/
+    public const ROLE_ADMIN_GROUP_ACTION_ALL          = 'ROLE_ADMIN_GROUP_ACTION_ALL';
+    public const ROLE_ADMIN_GROUP_ACTION_INDEX        = 'ROLE_ADMIN_GROUP_ACTION_INDEX';
+    public const ROLE_ADMIN_GROUP_ACTION_EDIT         = 'ROLE_ADMIN_GROUP_ACTION_EDIT';
+    public const ROLE_ADMIN_GROUP_ACTION_DELETE       = 'ROLE_ADMIN_GROUP_ACTION_DELETE';
+    public const ROLE_ADMIN_GROUP_ACTION_NEW          = 'ROLE_ADMIN_GROUP_ACTION_NEW';
+    public const ROLE_ADMIN_GROUP_ACTION_DETAIL       = 'ROLE_ADMIN_GROUP_ACTION_DETAIL';
+
+    /**
+     * 
+     * Roles owners
+     * You can just access your own entity informations if you have not this roles
+     *
+     */
+
+    /*************** -- ROLES OWNERS FOR ADMIN ENTITY -- ***************/
+    public const ROLE_ADMIN_OWNER_ALL           = 'ROLE_ADMIN_OWNER_ALL';
+    public const ROLE_ADMIN_OWNER_INDEX         = 'ROLE_ADMIN_OWNER_INDEX';
+    public const ROLE_ADMIN_OWNER_EDIT          = 'ROLE_ADMIN_OWNER_EDIT';
+    public const ROLE_ADMIN_OWNER_DELETE        = 'ROLE_ADMIN_OWNER_DELETE';
+    public const ROLE_ADMIN_OWNER_NEW           = 'ROLE_ADMIN_OWNER_NEW';
+    public const ROLE_ADMIN_OWNER_DETAIL        = 'ROLE_ADMIN_OWNER_DETAIL';
+
+    /*************** -- ROLES OWNERS FOR ADMIN_GROUP ENTITY -- ***************/
+    public const ROLE_ADMIN_GROUP_OWNER_ALL           = 'ROLE_ADMIN_GROUP_OWNER_ALL';
+    public const ROLE_ADMIN_GROUP_OWNER_INDEX         = 'ROLE_ADMIN_GROUP_OWNER_INDEX';
+    public const ROLE_ADMIN_GROUP_OWNER_EDIT          = 'ROLE_ADMIN_GROUP_OWNER_EDIT';
+    public const ROLE_ADMIN_GROUP_OWNER_DELETE        = 'ROLE_ADMIN_GROUP_OWNER_DELETE';
+    public const ROLE_ADMIN_GROUP_OWNER_NEW           = 'ROLE_ADMIN_GROUP_OWNER_NEW';
+    public const ROLE_ADMIN_GROUP_OWNER_DETAIL        = 'ROLE_ADMIN_GROUP_OWNER_DETAIL';
 
     public static function exists(?string $permissionName): bool
     {
@@ -23,7 +99,54 @@ final class PermissionsAdmin
         return \defined('self::' . $permissionName);
     }
 
-    public function getActionsAdminEntity()
+    public static function getAllRoles()
+    {
+        return [
+            'permissions.super_admin'                       => self::getSuperAdmin(),
+            'permissions.roles_allowed_to_edit_roles'       => self::getRoleAllowedToEditRoles(),
+            'permissions.roles_allowed_to_edit_groups'      => self::getRoleAllowedToEditGroups(),
+            'permissions.roles_allowed_to_switch'           => self::getRoleAllowedToSwitch(),
+            'permissions.actions.admin'                     => self::getActionsAdminEntity(),
+            'permissions.owners.admin'                      => self::getOwnersAdminEntity(),
+            'permissions.actions.admin_group'               => self::getActionsAdminGroupEntity(),
+            'permissions.owners.admin_group'                => self::getOwnersAdminGroupEntity(),
+        ];
+    }
+
+    /*************** -- Functions to pass to getAllRoles() -- ***************/
+    /***********************************************************************/
+    /**********************************************************************/
+    /*********************************************************************/
+
+    public static function getSuperAdmin()
+    {
+        return [
+            'ROLE_SUPER_ADMIN' => self::IS_ADMIN,
+        ];
+    }
+
+    public static function getRoleAllowedToEditRoles()
+    {
+        return [
+            'ROLE_ALLOWED_TO_EDIT_ROLES' => self::ROLE_ALLOWED_TO_EDIT_ROLES,
+        ];
+    }
+
+    public static function getRoleAllowedToEditGroups()
+    {
+        return [
+            'ROLE_ALLOWED_TO_EDIT_GROUPS' => self::ROLE_ALLOWED_TO_EDIT_GROUPS,
+        ];
+    }
+
+    public static function getRoleAllowedToSwitch()
+    {
+        return [
+            'ROLE_ALLOWED_TO_SWITCH' => self::ROLE_ALLOWED_TO_SWITCH,
+        ];
+    }
+
+    public static function getActionsAdminEntity()
     {
         return [
             'ROLE_ADMIN_ACTION_ALL' => self::ROLE_ADMIN_ACTION_ALL,
@@ -33,5 +156,79 @@ final class PermissionsAdmin
             'ROLE_ADMIN_ACTION_NEW' => self::ROLE_ADMIN_ACTION_NEW,
             'ROLE_ADMIN_ACTION_DETAIL' => self::ROLE_ADMIN_ACTION_DETAIL,
         ];
+    }
+
+    public static function getActionsAdminGroupEntity()
+    {
+        return [
+            'ROLE_ADMIN_GROUP_ACTION_ALL' => self::ROLE_ADMIN_GROUP_ACTION_ALL,
+            'ROLE_ADMIN_GROUP_ACTION_INDEX' => self::ROLE_ADMIN_GROUP_ACTION_INDEX,
+            'ROLE_ADMIN_GROUP_ACTION_EDIT' => self::ROLE_ADMIN_GROUP_ACTION_EDIT,
+            'ROLE_ADMIN_GROUP_ACTION_DELETE' => self::ROLE_ADMIN_GROUP_ACTION_DELETE,
+            'ROLE_ADMIN_GROUP_ACTION_NEW' => self::ROLE_ADMIN_GROUP_ACTION_NEW,
+            'ROLE_ADMIN_GROUP_ACTION_DETAIL' => self::ROLE_ADMIN_GROUP_ACTION_DETAIL,
+        ];
+    }
+
+    public static function getOwnersAdminEntity()
+    {
+        return [
+            'ROLE_ADMIN_OWNER_ALL' => self::ROLE_ADMIN_OWNER_ALL,
+            'ROLE_ADMIN_OWNER_INDEX' => self::ROLE_ADMIN_OWNER_INDEX,
+            'ROLE_ADMIN_OWNER_EDIT' => self::ROLE_ADMIN_OWNER_EDIT,
+            'ROLE_ADMIN_OWNER_DELETE' => self::ROLE_ADMIN_OWNER_DELETE,
+            'ROLE_ADMIN_OWNER_NEW' => self::ROLE_ADMIN_OWNER_NEW,
+            'ROLE_ADMIN_OWNER_DETAIL' => self::ROLE_ADMIN_OWNER_DETAIL,
+        ];
+    }
+
+    public static function getOwnersAdminGroupEntity()
+    {
+        return [
+            'ROLE_ADMIN_GROUP_OWNER_ALL' => self::ROLE_ADMIN_GROUP_OWNER_ALL,
+            'ROLE_ADMIN_GROUP_OWNER_INDEX' => self::ROLE_ADMIN_GROUP_OWNER_INDEX,
+            'ROLE_ADMIN_GROUP_OWNER_EDIT' => self::ROLE_ADMIN_GROUP_OWNER_EDIT,
+            'ROLE_ADMIN_GROUP_OWNER_DELETE' => self::ROLE_ADMIN_GROUP_OWNER_DELETE,
+            'ROLE_ADMIN_GROUP_OWNER_NEW' => self::ROLE_ADMIN_GROUP_OWNER_NEW,
+            'ROLE_ADMIN_GROUP_OWNER_DETAIL' => self::ROLE_ADMIN_GROUP_OWNER_DETAIL,
+        ];
+    }
+
+    /*************** -- Functions to have Authenticator checker -- ***************/
+    /****************************************************************************/
+    /***************************************************************************/
+    /**************************************************************************/
+
+    public static function checkAdmin($user)
+    {
+        if ($user->hasRole(self::IS_ADMIN)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function checkActions($user, $entity, $page)
+    {
+        $all = 'ROLE_' . $entity . '_ACTION_ALL';
+        $page = 'ROLE_' . $entity . '_ACTION_' . $page;
+
+        if ($user->hasRole($all) || $user->hasRole($page)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function checkOwners($user, $entity, $page)
+    {
+        $all = 'ROLE_' . $entity . '_OWNER_ALL';
+        $page = 'ROLE_' . $entity . '_OWNER_' . $page;
+
+        if ($user->hasRole($all) || $user->hasRole($page)) {
+            return true;
+        }
+
+        return false;
     }
 }
