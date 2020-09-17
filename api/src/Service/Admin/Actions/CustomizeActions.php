dfcdedfb86f6e23551fffe3782780bcd48a7cde1
John Dufrene <john.dufrene.pro@gmail.com>
2020-09-17 02:32:18 +0200

    Improve actions customizations + update dependencies

--- api/src/Service/Admin/Actions/CustomizeActions.php
+++ api/src/Service/Admin/Actions/CustomizeActions.php
@@ -6,10 +6,30 @@ use App\Entity\Security\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use Symfony\Component\Security\Core\Security;
 
 final class CustomizeActions
 {
     public const IMPERSONATE = 'impersonate';
+
+    /**
+     * security
+     *
+     * @var mixed
+     */
+    protected $security;
+
+    /**
+     * __construct
+     *
+     * @param  mixed $security
+     * @return void
+     */
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * all
      *
@@ -63,21 +83,61 @@ final class CustomizeActions
                 return $a->setIcon('fa fa-plus')->setLabel(false);
             })
             ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $a) {
-                return $a->setIcon('fa fa-edit')->setLabel(false);
+                return $a->setIcon('fa fa-edit')->setLabel(false)->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return true;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             })
             ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $a) {
-                return $a->setIcon('fa fa-trash')->setLabel(false);
+                return $a->setIcon('fa fa-trash')->setLabel(false)->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return false;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             })
             ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $a) {
-                return $a->setIcon('fa fa-eye')->setLabel(false);
+                return $a->setIcon('fa fa-eye')->setLabel(false)->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return true;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             });
 
         $actions
             ->update(Crud::PAGE_DETAIL, Action::EDIT, function (Action $a) {
-                return $a->setIcon('fa fa-edit')->setLabel(false);
+                return $a->setIcon('fa fa-edit')->setLabel(false)->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return true;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             })
             ->update(Crud::PAGE_DETAIL, Action::DELETE, function (Action $a) {
-                return $a->setIcon('fa fa-trash')->setLabel(false)->setCssClass('btn btn-danger');
+                return $a->setIcon('fa fa-trash')->setLabel(false)->setCssClass('btn btn-danger')->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return false;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             });
 
         $actions
@@ -85,17 +145,49 @@ final class CustomizeActions
                 return $a->setIcon('fa fa-list-alt')->setCssClass('btn btn-secondary');
             })
             ->update(Crud::PAGE_EDIT, Action::DETAIL, function (Action $a) {
-                return $a->setIcon('fa fa-eye')->setLabel(false)->setCssClass('btn btn-info');
+                return $a->setIcon('fa fa-eye')->setLabel(false)->setCssClass('btn btn-info')->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return true;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             })
 
             ->update(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE, function (Action $a) {
-                return $a->setIcon('fa fa-edit')->setCssClass('btn btn-success');
+                return $a->setIcon('fa fa-edit')->setCssClass('btn btn-success')->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return true;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             })
             ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $a) {
-                return $a->setIcon('fa fa-edit')->setLabel(false)->setCssClass('btn btn-success');
+                return $a->setIcon('fa fa-edit')->setLabel(false)->setCssClass('btn btn-success')->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return true;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             })
             ->update(Crud::PAGE_EDIT, Action::DELETE, function (Action $a) {
-                return $a->setIcon('fa fa-trash')->setLabel(false)->setCssClass('btn btn-danger');
+                return $a->setIcon('fa fa-trash')->setLabel(false)->setCssClass('btn btn-danger')->displayIf(function ($e) {
+                    if ($e instanceof Admin) {
+                        if ($this->security->getUser()->getId() === $e->getId()) {
+                            return false;
+                        }
+                        return $e->getIsAdmin() !== true;
+                    }
+                    return true;
+                });
             });
 
         $actions
@@ -115,6 +207,11 @@ final class CustomizeActions
         return $actions;
     }
 
+    /**
+     * impersonate
+     *
+     * @return void
+     */
     public function impersonate()
     {
         return Action::new(self::IMPERSONATE, 'Impersonate')
@@ -125,6 +222,15 @@ final class CustomizeActions
                     'id' => $e->getId(),
                     '_switch_user' => $e->getEmail()
                 ];
+            })
+            ->displayIf(function ($e) {
+                if ($e instanceof Admin) {
+                    if ($this->security->getUser()->getId() === $e->getId()) {
+                        return false;
+                    }
+                    return $e->getIsAdmin() !== true;
+                }
+                return true;
             });
     }
 }
