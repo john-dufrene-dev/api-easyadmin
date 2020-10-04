47a36bd4f01665a63d77237ba6e23a77160054a3
John Dufrene <john.dufrene.pro@gmail.com>
2020-10-04 16:55:26 +0200

    Add webpack Encore dependencie + create default route for app and admin

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -18,4 +18,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
